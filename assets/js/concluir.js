a6215bb993c5f3277e4d34458ed15246baf88c6a
Joao <joaocarloslima@me.com>
2023-10-19 07:30:28 -0300

    crud

---
+++ assets/js/concluir.js
@@ -0,0 +1,7 @@
+function concluir(id){
+    const tarefas = JSON.parse(localStorage.getItem("tarefas")) || []
+    let tarefa = tarefas.find(t => t.id === id)
+    tarefa.concluida = true
+    localStorage.setItem("tarefas", JSON.stringify(tarefas) )
+    atualizar()
+}
